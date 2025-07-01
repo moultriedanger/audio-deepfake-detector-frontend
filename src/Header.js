1ac9b1112a2a455471366ae310c809c76d3933b4
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-07-01 16:10:53 -0400

    migrated code to vite

--- src/Header.js
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-    return (
-        <div className="header-container">
-            <h1 id = "header-text">Fake or Real? An AI Powered Audio Deepfake Detector</h1>
-        </div>
-    )
-}
-export default Header;
